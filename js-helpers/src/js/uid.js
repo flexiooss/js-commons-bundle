add7851752d0f914eab76ce36c31dd1ecd315216
tomacht <thomas@flexio.fr>
2021-02-17 09:44:09 +0100

    fix add UID mini

--- js-helpers/src/js/uid.js
+++ js-helpers/src/js/uid.js
@@ -4,5 +4,20 @@
  * @function
  */
 export const UID = (prefix = '') => {
+  let dt = new Date().getTime()
+  let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = (dt + Math.random() * 16) % 16 | 0
+    dt = Math.floor(dt / 16)
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+  })
+  return prefix + uuid
+}
+
+/**
+ * @param {string} [prefix='']
+ * @return {string}
+ * @function
+ */
+export const UIDMini = (prefix = '') => {
   return prefix + Math.abs(((Math.random() * new Date()) | 0))
 }
