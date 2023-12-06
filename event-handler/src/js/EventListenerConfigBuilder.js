c1acaded03e194c9a7f7ea9e9f344890fb56a377
tomacht <thomas@flexio.fr>
2023-12-06 08:09:24 +0100

    typecheck

--- event-handler/src/js/EventListenerConfigBuilder.js
+++ event-handler/src/js/EventListenerConfigBuilder.js
@@ -68,7 +68,7 @@ export class EventListenerConfigBuilder {
   }
 
   /**
-   * @param {EventHandlerBase~eventClb} clb
+   * @param {function()} clb
    * @return {this}
    */
   onRemoveCallback(clb) {
